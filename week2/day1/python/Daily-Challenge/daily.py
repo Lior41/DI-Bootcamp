9bc36958a06d936cf094653311122d027c52db07
Lior41 <levlior41@gmail.com>
2023-11-26 22:34:44 +0200

    relevant message

---
+++ week2/day1/python/Daily-Challenge/daily.py
@@ -0,0 +1,17 @@
+
+user_input = input("H: ")
+
+if len(user_input) < 10:
+    print("String not long enough")
+elif len(user_input) > 10:
+    print("String too long")
+else:
+  
+    print("Perfect string")
+    print("First character:", user_input[0])
+    print("Last character:", user_input[-1])
+
+for i in range(1, len(user_input) + 1):
+        print(user_input[:i])
+        "HelloWorld"
+ 
