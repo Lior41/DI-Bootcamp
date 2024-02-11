055bd9cd21be167bb3ac11251232ee7817e1e61a
LIOR LEV <150783766+Lior41@users.noreply.github.com>
2024-02-11 18:01:16 +0200

    week14

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const fetchPosts = require('./fetch-data');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-fetchPosts();
+const quizRoutes = require('./quizRoutes');
+
+app.use('/', quizRoutes);
+
+app.listen(port, () => {
+  console.log(`Trivia quiz app listening at http://localhost:${port}`);
+});
